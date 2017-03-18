fb78a755d4320b14d2a7b750e5b5b72d8416970a
The Devils Voice <thedevilsvoice@tutanota.com>
2017-03-18 17:41:18 -0400

    arduino sketch cleanup

--- arduino_sketches/bsidesindy/bsidesindy.ino
+++ arduino_sketches/bsidesindy/bsidesindy.ino
@@ -1,37 +1,13 @@
-/***************************************************
-  Adafruit MQTT Library ESP8266 Example
-
-  Must use ESP8266 Arduino from:
-    https://github.com/esp8266/Arduino
-
-  Works great with Adafruit's Huzzah ESP board & Feather
-  ----> https://www.adafruit.com/product/2471
-  ----> https://www.adafruit.com/products/2821
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Tony DiCola for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-This example is for a 128x32 size display using I2C to communicate
-3 pins are required to interface (2 I2C and one reset)
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-*********************************************************************/
-
+/* 
+ *   ____  _           _       _         _     _           
+ *  / __ \(_)_ __   __| |_   _| |__  ___(_) __| | ___  ___ 
+ * / / _` | | '_ \ / _` | | | | '_ \/ __| |/ _` |/ _ \/ __|
+ *| | (_| | | | | | (_| | |_| | |_) \__ \ | (_| |  __/\__ \
+ * \ \__,_|_|_| |_|\__,_|\__, |_.__/|___/_|\__,_|\___||___/
+ *  \____/               |___/                             
+ *
+ *              H dlh dcaego uz aego ak.vol,vohs
+ */
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
@@ -42,88 +18,110 @@ All text above, and the splash screen must be included in any redistribution
 #include <Adafruit_SSD1306.h>
 
 
-/*************************
-  Adafruit.io Setup ******
-**************************/
-// #define AIO_SERVER "io.adafruit.com"
-#define AIO_SERVER      "6.6.6.1" 
+// Defines for LED pulse code
+#define BRIGHT    350     //max led intensity (1-500)
+#define INHALE    1250    //Inhalation time in milliseconds.
+#define PULSE     INHALE*1000/BRIGHT
+#define REST      100    //Rest Between Inhalations.
+
+/*
+ * Adafruit.io Setup 
+ * 
+ * Use this to connect to https://io.adafruit.com/
+ * rather than a local MQTT service. 
+ * 
+ * Create an ESP8266 WiFiClient class to connect to the MQTT server. 
+ * WiFiClient client; or... use WiFiFlientSecure for SSL
+ * 
+ */
+#define AIO_SERVER "io.adafruit.com"
 #define AIO_SERVERPORT  8883                   // use 8883 for SSL
 #define AIO_USERNAME    ""
 #define AIO_KEY         ""
+WiFiClientSecure client;
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
-#define OLED_RESET 2
-Adafruit_SSD1306 display(OLED_RESET);
 
-/********************** Global Variables Setup *******************************/
+
+/* 
+ * Global Variables Setup 
+ */
 char handle[ ] = "p0lr";
 char input[ ] = "";
 bool inputComplete = false;
-int switchPin = D3;              // switch is connected to pin D3
 const char* ssid = "batpack";
 const char* password = "batpack";
 //const char* mqtt_server = "broker.mqttdashboard.com";
 //const char* mqtt_server = "iot.eclipse.org";
 //const char* mqtt_server = "io.adafruit.com"
-
-/************ Global State (you don't need to change this!) ******************/
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-// WiFiClient client;
-// or... use WiFiFlientSecure for SSL
-WiFiClientSecure client;
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+#define OLED_RESET 2
+Adafruit_SSD1306 display(OLED_RESET);
+int switchPin1 = D9;              
+int switchPin2 = D10;
+int switchPin3 = D4;
+int switchPin4 = D3;
 
 /****************************** Feeds ***************************************/
 Adafruit_MQTT_Publish toServer = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/toServer");
 Adafruit_MQTT_Subscribe toBadge = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/toBadge");
 
 /*************************** Sketch Code ************************************/
-// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
-// for some reason (only affects ESP8266, likely an arduino-builder bug).
+
+/*  
+ * Bug workaround for Arduino 1.6.6, it seems to need a function declaration 
+ * for some reason (only affects ESP8266, likely an arduino-builder bug).
+ */
 void MQTT_connect();
 
 #if (SSD1306_LCDHEIGHT != 32)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-
 void setup() {
   Serial.begin(115200);
   delay(10);
   setup_display();
   setup_wifi();
-  pinMode(switchPin, INPUT);    // Set the switch pin as input
-  pinMode(LED_BUILTIN, OUTPUT); // initialize the on-board LED
-  
-  // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&toBadge);
+  pinMode(switchPin1,       INPUT);  // Set the switch pin as input
+  digitalWrite(switchPin1,  HIGH);   // turn on pull up resistor
+  pinMode(switchPin2,       INPUT);
+  digitalWrite(switchPin2,  HIGH);
+  pinMode(switchPin3,       INPUT);
+  digitalWrite(switchPin3,  HIGH);
+  pinMode(switchPin4,       INPUT);
+  digitalWrite(switchPin4,  HIGH);
+  pinMode(LED_BUILTIN,      OUTPUT); // initialize the on-board LED
+  digitalWrite(LED_BUILTIN, HIGH);   // turn LED off
+  mqtt.subscribe(&toBadge);          // Setup MQTT subscription for onoff feed.
 }
 
 void loop() {
   int status;
+  display.setCursor(0, 0);
+  display.println("@indybsides");
+  display.display();
   
   MQTT_connect();
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000))) {
     if (subscription == &toBadge) {
-      //Serial.println((char *)toBadge.lastread);
+      Serial.println((char *)toBadge.lastread);
     }
   }
 
-  // Check for any button press
-  int val = digitalRead(switchPin);   // read input value and store it in val
-  String msg="Button pressed";
-  if (val == HIGH) {               // check if the button is pressed
-    digitalWrite(LED_BUILTIN, LOW);   // turn LED on
-    String msg="Button pressed";
-    Serial.println(msg);
-    display.println(msg);
-    display.display();
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);  // turn LED off
-    delay(500);
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH);
+  // check if the button is pressed
+  if (digitalRead(switchPin1) == LOW) {               
+    button_press("1");
+  } else if (digitalRead(switchPin2) == LOW) {
+    button_press("2");
+  } else if (digitalRead(switchPin3) == LOW) {
+    button_press("3");
+  } else if (digitalRead(switchPin4) == LOW) {
+    button_press("4");
+  }
+  else {
+
     Serial.println("Nothing just happened");
   }
   
@@ -140,7 +138,7 @@ void loop() {
 
   // Now we can publish stuff!
   if (inputComplete) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    //digitalWrite(LED_BUILTIN, HIGH);
     char post[ ] = "";
     strcat(post, handle);
     strcat(post, ": ");
@@ -188,8 +186,11 @@ void setup_wifi() {
   display.display();
 }
 
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
+/* 
+ * Function to connect and reconnect as necessary to the MQTT server.
+ * Should be called in the loop function and it will take care if connecting.
+ * 
+ */
 void MQTT_connect() {
   int8_t ret;
 
@@ -221,3 +222,87 @@ void MQTT_connect() {
   display.display();
   
 }
+
+void button_press(String button_num) {
+  //digitalWrite(LED_BUILTIN, LOW);   // turn LED on
+  String msg = "Button " + button_num + " pressed";
+  Serial.println(msg);
+  display.println(msg);
+  display.display();
+  led_half_breath();
+  delay(300);
+  //digitalWrite(LED_BUILTIN, HIGH);  // turn LED off
+  digitalWrite(switchPin1, HIGH);
+  digitalWrite(switchPin2, HIGH);
+  digitalWrite(switchPin3, HIGH);
+  digitalWrite(switchPin4, HIGH);
+  delay(200);
+  display.setCursor(0, 4);
+  display.clearDisplay();
+  display.display();
+}
+
+/*
+ * Pulse the Internal LED
+ */
+void led_breath() {
+     //ramp increasing intensity, Inhalation: 
+  for (int i=1;i<BRIGHT;i++){
+    digitalWrite(LED_BUILTIN, LOW);          // turn the LED on.
+    delayMicroseconds(i*10);         // wait
+    digitalWrite(LED_BUILTIN, HIGH);         // turn the LED off.
+    delayMicroseconds(PULSE-i*10);   // wait
+    delay(0);                        //to prevent watchdog firing.
+  }
+}
+
+/*
+ * Half-pulse the internal LED
+ */
+void led_half_breath() {
+  
+  //ramp decreasing intensity, Exhalation (half time):
+  for (int i=BRIGHT-1;i>0;i--){
+    digitalWrite(LED_BUILTIN, LOW);          // turn the LED on.
+    delayMicroseconds(i*10);          // wait
+    digitalWrite(LED_BUILTIN, HIGH);         // turn the LED off.
+    delayMicroseconds(PULSE-i*10);  // wait
+    i--;
+    delay(0);                        //to prevent watchdog firing.
+  }
+  delay(REST);                       //take a rest...
+}
+
+/***************************************************
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board & Feather
+  ----> https://www.adafruit.com/product/2471
+  ----> https://www.adafruit.com/products/2821
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x32 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
